f841624721f57af7f8cad9f265af7d7cb0c4868e
godwillcodes <iamgodwillb@gmail.com>
2024-05-27 02:18:05 +0300

    remove redundant files for easy corelation

--- resources/views/single-gallery.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('components.layouts.app')
-
-@section('title', 'Energy PS, Hon. Dr. Eng. Joseph .K. Njoroge with a member of staff on site inspecting parts of broken conductors at a coffee plantation in Cianda area, Kiambu County.')
-    @livewire('banner')
-    @livewire('breadcrumb')
-    @livewire('single-gallery-list') 
-    @livewire('latest-gallery')
-    @livewire('footer')
-
